4b36e8dda2a79e557db6a4dd701b2d00a61d8723
Mayall <KaylemWood@outlook.com>
2020-04-06 08:32:35 +0100

    Corrected copyright date.

--- RemoveOverheatable/Properties/AssemblyInfo.cs
+++ RemoveOverheatable/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("RemoveOverheatable")]
 [assembly: AssemblyProduct("RemoveOverheatable")]
-[assembly: AssemblyCopyright("Copyright Mayall 2019")]
+[assembly: AssemblyCopyright("Copyright Mayall 2020")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.1.0")]
 [assembly: NeutralResourcesLanguage("en")]

d772da72cad4153468c44d8f219d5c7117dc51bb
Mayall <KaylemWood@outlook.com>
2020-04-10 12:28:24 +0100

    Integrated overlay sync support.

--- Blueprints/BlueprintsOptions.cs
+++ Blueprints/BlueprintsOptions.cs
@@ -2,8 +2,15 @@
 using PeterHan.PLib;
 
 namespace Blueprints {
+    public enum DefaultSelections {
+        All, None
+    }
+
     [JsonObject]
     public class BlueprintsOptions {
+        [Option("Default Menu Selections", "The default selections made when an advanced filter menu is opened.")]
+        public DefaultSelections DefaultMenuSelections { get; set; } = DefaultSelections.All;
+
         [Option("Require Constructable", "Whether buildings must be constructable by the player to be used in blueprints.")]
         public bool RequireConstructable { get; set; } = true;
 
@@ -12,5 +19,11 @@ namespace Blueprints {
 
         [Option("FX Time", "How long FX created by Blueprints remain on the screen. Measured in seconds.")]
         public float FXTime { get; set; } = 4;
+
+        [Option("Blueprint Tool Overlay Sync", "Whether the Blueprint Tool syncs with the current overlay. (configurable in game too)")]
+        public bool CreateBlueprintToolSync { get; set; } = true;
+
+        [Option("Snapshot Tool Overlay Sync", "Whether the Snapshot Tool syncs with the current overlay. (configurable in game too)")]
+        public bool SnapshotToolSync { get; set; } = true;
     }
 }

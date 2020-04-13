a633578dc61828b0ec9dba070b9d0b35296f6666
Mayall <KaylemWood@outlook.com>
2020-04-13 09:26:01 +0100

    Incremented version.

--- AdvancedFilterMenu/Properties/AssemblyInfo.cs
+++ AdvancedFilterMenu/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("AdvancedFilterMenu")]
 [assembly: AssemblyCopyright("Copyright Mayall 2020")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.2.11.0")]
+[assembly: AssemblyVersion("1.2.12.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
